8e4181dde76c737be8b489ba7b1894b84fb2494c
NuclearGandhi <dodobentov@gmail.com>
2025-03-24 09:55:21 +0200

    Enhance blockquote handling and introduce new box types for improved formatting

--- quartz/plugins/custom-rebber/blockquote.ts
+++ quartz/plugins/custom-rebber/blockquote.ts
@@ -1,20 +1,26 @@
-const defaultMacro = (innerText: string) => `\\begin{Quotation}\n${innerText}\n\\end{Quotation}\n\n`;
+const defaultMacro = (innerText: string) => {
+    if (innerText.trim() === '') return '';
+    return `\\begin{SimpleBox}\n${innerText}\n\\end{SimpleBox}\n\n`;
+};
 
 /* Stringify a Blockquote `node`. */
 export function blockquote(all: Function, one: Function, ctx: Context, node: Node) {
-    if (!node.children) return defaultMacro(all(ctx, node));
+    if (!node.children) {
+        return defaultMacro(all(ctx, node));
+    }
 
     // First child may be an html node, and if it is, then its the title, and we should pass it as
     // the title to the blockquote
     const titleNode = node.children[0].type === 'html' ? node.children[0] : null;
     const titleText = titleNode ? one(ctx, titleNode) : '';
 
-    // Remove the title node from the children
-    node.children = node.children.slice(1);
-
+    // Remove the title node from the children if there is a title
+    if (titleNode) {
+        node.children.shift();
+    }
 
     const innerText = all(ctx, node);
-
+    
     // Return a latex Callout according to the node.hProperties.data-callout
     if (node.data && node.data.hProperties && node.data.hProperties['data-callout']) {
         const calloutType = node.data.hProperties['data-callout'];
@@ -25,12 +31,14 @@ export function blockquote(all: Function, one: Function, ctx: Context, node: Nod
 }
 
 function calloutLatex(calloutType: string, titleText: string, innerText: string) {
+    if (innerText.trim() === '') return '';
     var boxName;
     switch (calloutType) {
         case 'example':
             boxName = 'ExampleBox';
             break;
         case 'notes':
+        case 'note':
             boxName = 'NotesBox';
             break;
         case 'question':
@@ -40,14 +48,33 @@ function calloutLatex(calloutType: string, titleText: string, innerText: string)
             boxName = 'WarningBox';
             break;
         case 'error':
+        case 'failure':
+        case 'danger':
+        case 'bug':
             boxName = 'ErrorBox';
             break;
         case 'info':
+        case 'todo':
             boxName = 'InfoBox';
             break;
-        default:
-            boxName = 'Quotation';
+        case 'success':
+            boxName = 'SuccessBox';
             break;
+        case 'tip':
+            boxName = 'TipBox';
+            break;
+        case 'abstract':
+        case 'new':
+            boxName = 'AbstractBox';
+            break;
+        case 'theorem':
+            boxName = 'TheoremBox';
+            break;
+        case 'quote':
+            boxName = 'QuotationBox';
+            break;
+        default:
+            return defaultMacro(innerText);
     }
 
     return `\\begin{Callout}{${boxName}}[${titleText}]\n${innerText}\\end{Callout}\n\n`;
